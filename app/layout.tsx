468806fdc3ed24fc844da66a30c4bd5dc8c145a1
Hritik Hassani <hassani.h@northeastern.edu>
2026-07-13 14:06:14 -0400

    updating analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Analytics from "@/components/Analytics";
 
 const SITE = "https://hritikhassani.com";
 
@@ -73,7 +74,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           dangerouslySetInnerHTML={{ __html: JSON.stringify(personSchema) }}
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
